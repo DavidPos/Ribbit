177f9c852c0cb7d90c3223b997f53a1651ecc77d
David Pos <bigdavepos@aol.com>
2014-11-18 17:37:59 -0800

    Login
    Start the login in function once user has signed up.

--- app/src/main/java/sailloft/ribbit/Login_Activity.java
+++ app/src/main/java/sailloft/ribbit/Login_Activity.java
@@ -1,21 +1,88 @@
 package sailloft.ribbit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class Login_Activity extends Activity {
     protected TextView mSignUpTextView;
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_);
+        mUsername = (EditText)findViewById(R.id.usernameField);
+        mPassword = (EditText)findViewById(R.id.passwordField);
+
+        mLoginButton = (Button)findViewById(R.id.loginButton);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+
+                username.trim();
+                password.trim();
+
+
+                if (username.isEmpty() || password.isEmpty() ){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(Login_Activity.this);
+                    builder.setMessage(R.string.)
+                            .setTitle(R.string.)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    //create new user
+                    ParseUser newUser = new  ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setPassword(password);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null){
+                                //Success
+                                Intent intent = new Intent(Login_Activity.this,MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+
+                            }
+                            else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Login_Activity.this);
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.sign_up_error_title)
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+
+                }
+            }
+
+        });
+
+
+
 
         mSignUpTextView = (TextView)findViewById(R.id.signUpText);
         mSignUpTextView.setOnClickListener(new View.OnClickListener() {
