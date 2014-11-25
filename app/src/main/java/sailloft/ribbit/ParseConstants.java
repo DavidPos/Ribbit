9c808a807e2d6e4309f032095b5776b163e4cb3e
David Pos <bigdavepos@gmail.om>
2014-11-25 07:45:57 -0800

    Add Friends
    edit friends added to enable finding friends

---
+++ app/src/main/java/sailloft/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package sailloft.ribbit;
+
+/**
+ * Created by David's Laptop on 11/25/2014.
+ */
+public final class ParseConstants {
+    //Class Name
+
+
+    //Field names
+    public static final String KEY_USERNAME = "username";
+}
